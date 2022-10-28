da9417fc284b49009f5e93e7744d34ccd750169e
ElizaKim <76706219+ElizaKim@users.noreply.github.com>
2022-10-27 21:26:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelectorAll("#menu-icon");
+let navbar = document.querySelector("#.navbar");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};
